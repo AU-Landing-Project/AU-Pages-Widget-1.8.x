c3429b5d0f3d01beaef6274d3ae99879fd8efbbd
Matt Beckett <beck24@gmail.com>
2012-07-03 10:49:58 -0600

    made widgets multiple

--- start.php
+++ start.php
@@ -1,6 +1,8 @@
 <?php
 
-// no setup required here
-// all taken care of in the au_widgets_framework
-// only thing here is views for the widget
-// start.php is required for the plugin skeleton however.
+function au_pages_widget_init(){
+	elgg_unregister_widget_type('pages');
+	elgg_register_widget_type('pages', elgg_echo('pages'), elgg_echo('pages:widget:description'), 'all,groups', TRUE);
+}
+
+elgg_register_event_handler("init", "system", "au_pages_widget_init");
